9012d696e93f607b19a2cb4a4ca813d14fa47029
YMP-BLACRUSH <67401418+YMP-BLACRUSH@users.noreply.github.com>
2021-12-19 19:48:53 +0700

    Adding Images src and Basic Settings

---
+++ components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from '../styles/Navbar.module.css'
+
+const Navbar = () => {
+    return (
+        <div className={styles.container}>
+            Navbar
+        </div>
+    )
+}
+
+export default Navbar
+Navbar
