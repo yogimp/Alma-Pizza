1d737f0a9afb63343d2e74b6a49c12027a3bbe85
yogimp <yogimeizalputra@gmail.com>
2023-02-18 16:56:53 +0700

    Adding Pizza List

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
 import Featured from '@/components/Featured'
+import PizzaList from '@/components/PizzaList'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,6 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Featured />
+      <PizzaList />
     </>
   )
 }
